d01306e06e21fd04a823071097d28b35158f72d6
riotDev17 <tomasalberto527@gmail.com>
2024-02-20 22:06:11 +0700

    TS

--- dist/tests/hello.test.js
+++
@@ -1,6 +0,0 @@
-import { sayHello } from '../src/hello';
-describe('say hello test', () => {
-    it('should say Hello Thomas Alberto', () => {
-        expect(sayHello('Thomas Alberto')).toBe('Hello Thomas Alberto');
-    });
-});
